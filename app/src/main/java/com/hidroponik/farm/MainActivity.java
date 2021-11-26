0bb3e0e04c6510c87e83db786c35338d5815076b
Reynaldo <koesanto.r@gmail.com>
2021-11-26 23:02:02 +0700

    update java

--- app/src/main/java/com/hidroponik/farm/MainActivity.java
+++ app/src/main/java/com/hidroponik/farm/MainActivity.java
@@ -1,14 +1,73 @@
 package com.hidroponik.farm;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView SensorPpm, SensorPh, SensorSuhu, SetPt, PomABMix, PomAir, PomPhUp, PomPhD;
+    private SeekBar SeekTds;
+
+    int cMax = 1400, cStep = 50, cProg, pProg, nProg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SensorPpm = (TextView) findViewById(R.id.ppm);
+        SensorPh = (TextView) findViewById(R.id.ph);
+        SensorSuhu = (TextView) findViewById(R.id.suhu);
+        SetPt = (TextView) findViewById(R.id.setpoint);
+        PomABMix = (TextView) findViewById(R.id.p_abmix);
+        PomAir = (TextView) findViewById(R.id.p_air);
+        PomPhUp = (TextView) findViewById(R.id.p_phup);
+        PomPhD = (TextView) findViewById(R.id.p_phdn);
+        SeekTds = (SeekBar) findViewById(R.id.seek_ppm);
+
+        SeekTds.setMax(cMax / cStep);
+        SeekTds.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                cProg = progress * cStep;
+                nProg = cProg;
+                SetPt.setText("" + nProg);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle("Konfirmasi");
+                alert.setMessage("Ubah setpoint TDS menjadi " + SetPt.getText() + " PPM");
+                alert.setCancelable(false);
+                alert.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        SeekTds.setProgress(nProg);
+                        nProg = pProg;
+                        Toast.makeText(MainActivity.this, "Setpoint berhasil di perbaharui", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alert.setNegativeButton("Batal", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+//                        SetPt.setText("" + pProg);
+                        SeekTds.setProgress(pProg);
+                        Toast.makeText(MainActivity.this, "Setpoint batal di perbaharui", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alert.show();
+            }
+        });
     }
 }
