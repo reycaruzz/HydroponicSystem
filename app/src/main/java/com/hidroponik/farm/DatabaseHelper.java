78f39f7bec2b88a1bc7382c0f70d7e29588fe4de
Reynaldo <r.koesanto@outlook.com>
2022-02-11 21:08:22 +0700

    gradle patched to 7.1.1

--- app/src/main/java/com/hidroponik/farm/DatabaseHelper.java
+++ app/src/main/java/com/hidroponik/farm/DatabaseHelper.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
 
 import java.util.Locale;
 
